ab4adbf48d5d6a2d6975f236d730b49b0824d545
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-27 09:57:22 -0600

    Fix bug to add new task

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,18 +1,20 @@
 function getTaskData() {
-  { name: $("#name").val(); }
+  return  { name: $("#name").val() };
 };
 
 function createTask() {
-  var taskData = getTaskData();
-  $.post("/tasks",
-       taskData,
-       function(data) {
-           $("#tasks").append(data);
-           $("#name").val("");
-       });
+    var taskData = getTaskData();
+    $.post("/tasks",
+        taskData,
+        function (data) {
+            $("#tasks").append(data);
+            $("#name").val("");
+        });
 };
 
-$("#submit-task").submit(function(event) {
-  event.preventDefault();
-  createTask();
+$('document').ready(function () {
+    $("#submit-task").submit(function (event) {
+        event.preventDefault();
+        createTask();
+    });
 });
